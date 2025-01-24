06b6f4b06efdee7823dfc38c33b9d9c9e31b75d9
Jedrzej <jpt.graczykowski@gmail.com>
2025-01-24 10:08:29 +0100

    26 add routes for the line item dates

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, except: [:index, :show]
+  end
 end
